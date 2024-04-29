2d5f3c78e684246dffe24896bb3e4b74ab0e1582
Burcu BOLAT AKÇA <bbakca@kho.edu.tr>
2024-04-29 13:45:32 +0300

    Stil verme çeşitleri

---
+++ HTMLLesson/Controllers/userController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HTMLLesson.Controllers
+{
+    public class userController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
--- HTMLLesson/Program.cs
+++ HTMLLesson/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=User}/{action=Register}/{id?}"); //Programýn default açýlacaðý yeri belirledik
 
 app.Run();
