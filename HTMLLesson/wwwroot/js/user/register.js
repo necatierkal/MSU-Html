0dd110f9159f9c3d31d450e45d5b5e884e326e5b
Burcu BOLAT AKÇA <bbakca@kho.edu.tr>
2024-04-30 10:26:01 +0300

    Stil verme çeşitleri

--- HTMLLesson/wwwroot/js/user/register.js
+++ HTMLLesson/wwwroot/js/user/register.js
@@ -1,6 +1,33 @@
 ﻿
+$(document).ready(function () {
+    Page.Init();
+});
+
+let Page = {
+    Init: function () {
+        Page.Handler();
+        Page.Clear();
+    },
+    Handler: function () {
+        $(document).on("click", "#btn_cancel", function (e) {
+            history.back(); //Javascript metodudur. 1 geriye gelir. İçerisine rakam belirtirsek o kadar geriye gelir.
+        });
+        $(document).on("click", "#btn_clear", function (e) {
+            Page.Clear();
+        });
+    },
+    Clear: function () {
+    //    document.getElementsByClassName("Form-control").val = "";
+    //    document.getElementsById("input_lastname").val = "";
+
+        $("body :input").val(""); //body etiketinenin içerisindeki tüm inputlarını al, valuelarına boş atama yap. (Clear)
+        $("#input_username").focus();
+    }
+
+}
 
 console.log("Register javascript loaded.");
-$(document).on("click", "#btn_clear", function (e){
-    console.log("Clear button clicked.");
-});
+
+//$(document).on("click", "#btn_clear", function (e) {
+//    console.log("Clear button clicked.");
+//});
